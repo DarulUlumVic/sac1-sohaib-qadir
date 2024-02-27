9da36d97a2b0775ff7e6caa2ada52d6622f411a3
TeacherAtDU <139174572+TeacherAtDU@users.noreply.github.com>
2024-02-28 09:33:52 +1100

    Create SAC1.py

---
+++ SAC1.py
@@ -0,0 +1,20 @@
+'''
+Task1
+You're a junior developer at a financial services company tasked with building a 
+program to calculate compound interest for client investments. You've been provided 
+with a formula and test data to guide your implementation.
+
+Pseudo code:
+
+FUNCTION compoundInterest(principal, annualInterestRate, compoundingFrequency, time)
+BEGIN
+    amountAccumulated <-- principal * (1 + (annualInterestRate / compoundingFrequency))^(compoundingFrequency * time)
+    RETURN amountAccumulated
+END FUNCTION
+
+
+1 Implement a function based on the provided compound interest formula.
+2 Write code to utilize this function, passing in values from the test data, and display the calculated compound interest.
+3 Construct a test table using a word processing tool, filling it with the provided test data and expected results.
+
+'''
